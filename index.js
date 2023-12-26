671df7ce1d1844ffc5f4556f016cb37ac7986f15
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-26 18:22:29 +0100

    [update] index.js

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 const port = 3002;
+const path = '';
 
 app.use(express.json());
 app.use(cors());
 
-app.post('', (req, res) => {});
+function getPath(name) {
+	nameList = fs.writeFileSync(path + 'main.json');
+
+	jsonPath = nameList[name];
+	return JSON.parse(fs.readFileSync(path + jsonPath));
+}
+
+app.post('', (req, res) => {
+	getPath()
+		.then((containerList) => {
+			res.json(containerList);
+		})
+		.catch((error) => {
+			res.sendStatus(404);
+			res.send(error);
+		});
+	res.json();
+});
 
 app.listen(port, () => console.log(`The SYS-ManageData server runs on port ${port}`));
